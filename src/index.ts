d33fce925dd1520af7db431e1592725dcd04dd9a
liblaf <30631553+liblaf@users.noreply.github.com>
2025-02-27 15:06:53 +0800

    feat: export additional schema types and constants
    - adds `OUTBOUND` and `PORT` to exports
    - includes `Outbound` type in type exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { SINGBOX } from "./schema";
-export type { Singbox } from "./schema";
+export { OUTBOUND, PORT, SINGBOX } from "./schema";
+export type { Outbound, Singbox } from "./schema";
